b7d25224da24621878b2eb111287a85643ee212f
thinknathan <github@thinknathan.com>
2024-10-14 14:11:37 -0700

    chore: remove eslint

--- src/modules/template.ts
+++ src/modules/template.ts
@@ -5,7 +5,6 @@ export class Template {
   boasting: Array<string> = [
     "  ⭐ .vscode folder based project",
     "  ⭐ lua-types and defold-types for autocomplete and inline docs",
-    "  ⭐ eslint with sane typescript defaults",
     "  ⭐ patched typescript-to-lua to generate *.script files",
     "  ⭐ @ts-defold/tstl-export-as-global plugin for rewriting module exports",
   ];
